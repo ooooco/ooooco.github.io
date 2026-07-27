0689b3b777763f95ae062896db810212c6844838
ooooco <ovooxc@gmail.com>
2026-07-27 17:54:54 +0800

    Site updated: 2026-07-27 17:54:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/27/Hello-World/"];function toRandomPost(){
+var posts=["2026/07/27/这是一篇新的博文/","2026/07/27/Hello-World/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
