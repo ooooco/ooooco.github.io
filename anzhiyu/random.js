0be8d5a67b496a3bf1a84343d405c0a954eae292
ooooco <ovooxc@gmail.com>
2026-07-29 18:59:46 +0800

    Site updated: 2026-07-29 18:59:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/28/players/","2026/07/27/crosshair-library/"];function toRandomPost(){
+var posts=["2026/07/29/瓦学家_开篇/","2026/07/28/players/","2026/07/27/crosshair-library/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
